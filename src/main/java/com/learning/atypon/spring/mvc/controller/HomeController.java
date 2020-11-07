592994600608e6434e6beef15074773fd3a331c6
gpapadakis <gpapadakis@atypon.com>
2020-11-07 10:24:19 +0200

    master: Add project structure

--- src/main/java/com/learning/atypon/spring/mvc/controller/HomeController.java
+++ src/main/java/com/learning/atypon/spring/mvc/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping({"/"})
     public String home(Model model) {
-        model.addAttribute("username", "Stelios");
+        model.addAttribute("username", "Test Automation Engineer");
         return "/index";
     }
 }
